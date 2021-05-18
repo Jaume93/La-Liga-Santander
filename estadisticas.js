19415c062a3f09c565e7d2976cfa6f316e563c34
Jaume Córdoba <jaumec93@gmail.com>
2021-05-18 21:41:19 +0200

    acabado estadisticas

--- estadisticas.js
+++ estadisticas.js
@@ -1,5 +1,4 @@
 
-
 tableStatistics(data1.matches)
 
 function tableStatistics(matches) {
@@ -20,6 +19,7 @@ function tableStatistics(matches) {
         let foundAwayTeam
 
         for (let j = 0; j < statistic.length; j++) {
+
             if (idHome == statistic[j].id) {
                 foundHomeTeam = statistic[j]
             }
@@ -41,7 +41,6 @@ function tableStatistics(matches) {
             foundHomeTeam.matches++;
         }
 
-
         if (foundAwayTeam == undefined) {
             let objeto = {
                 id: idAway,
@@ -54,30 +53,142 @@ function tableStatistics(matches) {
             foundAwayTeam.goals += goalsAway;
             foundAwayTeam.matches++;
         }
-    } console.log(statistic)
+
+    }
+    for (let i = 0; i < statistic.length; i++) {
+        statistic[i].avg = statistic[i].goals / statistic[i].matches;
+    }
+    console.log(statistic)
+
+    // statistic.sort((a, b) => b.avg - a.avg);
+
+    createTable(statistic)
 };
 
+function createTable(tableAvgGoals) {
 
-// 0. Crear función que va calcular las estadísticas, recibiendo como param el array de partidos:
+    tableAvgGoals.sort((a, b) => b.avg - a.avg);
 
-// 1. Crear array vacía (será array de objetos)
+    const tableBody = document.getElementById('table_body');
 
-// 2. Iterar por todos los partidos
 
-// 3. Condición: si el partido no está acabado, no seguir y mirar siguiente partido, si no el null 
-// de los goles lo romperá todo.
+    for (let i = 0; i < 5; i++) {
+        let row = document.createElement('tr');
 
-// 4. Buscar en la array estadísticas el objeto con el mismo id que el homeTeam del partido y guardarlo en una variable
+        let equipos = tableAvgGoals[i].id
+        let imgClub = document.createElement('img');
+        imgClub.classList.add('imgClub')
+        imgClub.setAttribute('src', "https://crests.football-data.org/" + equipos + ".svg");
 
-// 5. Si el objeto buscado no existe, crearlo con estos keys: id, name, goals, matches.
-// Rellenar cada key con el valor correspondiente
+        let club = tableAvgGoals[i].name;
+        let goals = tableAvgGoals[i].goals;
+        let matches = tableAvgGoals[i].matches;
+        let avg = tableAvgGoals[i].avg.toFixed(3)
 
-// 6. Si existe, actualizar los goles y los partidos
+        let dataClubs = [imgClub, club, goals, matches, avg]
 
-// 7. Hacer exactamente lo mismo a partir del punto 4, pero con awayTeam
+        for (let j = 0; j < dataClubs.length; j++) {
+            let celda = document.createElement('td');
+            row.append(celda)
+            celda.append(dataClubs[j])
+        }
+        tableBody.append(row);
+    }
+}
 
-// 8. Una vez fuera del loop de partidos, iterar por el array estadisticas
 
-// 9. Añadir la key avg a cada objeto, con el valor goals/matches
 
-// 10. Hacer console.log() para ver que todo está correcto.
+tableStatistics2(data1.matches)
+
+function tableStatistics2(matches) {
+
+    let statistic2 = [];
+
+    for (let i = 0; i < matches.length; i++) {
+        if (matches[i].status != "FINISHED") {
+            continue
+        }
+        // let idHome = matches[i].homeTeam.id;
+        let idAway = matches[i].awayTeam.id;
+        // let nameTeamHome = matches[i].homeTeam.name;
+        let nameTeamAway = matches[i].awayTeam.name;
+        let goalsHome = matches[i].score.fullTime.homeTeam;
+        // let goalsAway = matches[i].score.fullTime.awayTeam;
+        let foundHomeTeam
+        let foundAwayTeam
+
+        for (let j = 0; j < statistic2.length; j++) {
+
+            // if (idHome == statistic[j].id) {
+            //     foundHomeTeam = statistic[j]
+            // }
+            if (idAway == statistic2[j].id) {
+                foundAwayTeam = statistic2[j]
+            }
+        }
+
+        // if (foundHomeTeam == undefined) {
+        //     let objeto = {
+        //         id: idHome,
+        //         name: nameTeamHome,
+        //         goalsAgainst: goalsHome,
+        //     }
+        //     statistic.push(objeto);
+        // } else {
+        //     foundHomeTeam.goalsAgainst += goalsHome;
+        //     foundHomeTeam.matches++;
+        // }
+
+        if (foundAwayTeam == undefined) {
+            let objeto = {
+                id: idAway,
+                name: nameTeamAway,
+                goalsAgainst: goalsHome,
+                matches: 1
+            }
+            statistic2.push(objeto);
+        } else {
+            foundAwayTeam.goalsAgainst += goalsHome;
+            foundAwayTeam.matches++;
+        }
+
+    }
+    for (let i = 0; i < statistic2.length; i++) {
+        statistic2[i].avgGoalsAway = statistic2[i].goalsAgainst / statistic2[i].matches;
+    }
+    console.log(statistic2)
+
+    // statistic.sort((a, b) => b.avg - a.avg);
+
+    createTable2(statistic2)
+};
+
+function createTable2(tableGoalsAgainst) {
+
+    tableGoalsAgainst.sort((a, b) => a.avgGoalsAway - b.avgGoalsAway);
+
+    const tableBody2 = document.getElementById('table_body2');
+
+    for (let i = 0; i < 5; i++) {
+        let row = document.createElement('tr');
+
+        let equipos = tableGoalsAgainst[i].id
+        let imgClub = document.createElement('img');
+        imgClub.classList.add('imgClub')
+        imgClub.setAttribute('src', "https://crests.football-data.org/" + equipos + ".svg");
+
+        let club = tableGoalsAgainst[i].name;
+        let matches = tableGoalsAgainst[i].matches;
+        let goalsAgainst = tableGoalsAgainst[i].goalsAgainst;
+        let avgGoalsAway = tableGoalsAgainst[i].avgGoalsAway.toFixed(3);
+
+        let dataClubs = [imgClub, club, goalsAgainst, matches, avgGoalsAway]
+
+        for (let j = 0; j < dataClubs.length; j++) {
+            let celda = document.createElement('td');
+            row.append(celda)
+            celda.append(dataClubs[j])
+        }
+        tableBody2.append(row);
+    }
+}
